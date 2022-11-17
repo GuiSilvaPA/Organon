fa9a63eca0c47ddcf06865e6db3c605b8cb3f9dd
GuiSilvaPA <77038270+GuiSilvaPA@users.noreply.github.com>
2022-11-17 08:14:12 -0300

    Newest version

--- Organon.py
+++ Organon.py
@@ -3,10 +3,11 @@ import numpy as np
 
 class NetworkData():
     
-    def __init__(self, path):
+    def __init__(self, path, other=False):
         
-        self.path = path
-        self.time = 15.0
+        self.path  = path
+        self.time  = 15.0
+        self.other = other
         
         with open(path) as f:
             self.lines = f.readlines()
@@ -26,6 +27,8 @@ class NetworkData():
         self.getBusDataFrame()
         self.getLoadDataFrame()
         self.getGenDataFrame()
+        
+        
 
     ### ==========================================================================================================================
     '''
@@ -37,6 +40,12 @@ class NetworkData():
 
         columns = ['BUS_ID', 'BUS_NAME', 'VBASEKV', 'TP', 'S', 'GSHT_MW', 'BSHMVAR', 'ARE', 'ZON',
                   'MODV_PU', 'ANGV_DEG', 'VMXN_PU', 'VMNN_PU', 'VMXE_PU', 'VMNE_PU', 'OWN', 'SUB', 'ARG']
+        
+        if self.other:
+            columns = ['BUS_ID', 'BUS_NAME', 'VBASEKV', 'TP', 'S', 'GSHT_MW', 'BSHMVAR', 'ARE', 'ZON',
+                       'MODV_PU', 'ANGV_DEG', 'VMXN_PU', 'VMNN_PU', 'OWN', 'SUB']
+        
+        
         data    = []
 
         for i in range(self.f_bus, self.l_bus + 1):
@@ -63,6 +72,23 @@ class NetworkData():
                      'OWN'      : 'int32',
                      'SUB'      : 'int32',
                      'ARG'      : 'int32'}
+        
+        if self.other:
+            type_dict = {'BUS_ID'   : 'int32',
+                         # 'BUS_NAME' :,
+                         'VBASEKV'  : 'float64',
+                         'TP'       : 'int32',
+                         'S'        : 'int32',
+                         'GSHT_MW'  : 'float64',
+                         'BSHMVAR'  : 'float64',
+                         'ARE'      : 'int32',
+                         'ZON'      : 'int32',
+                         'MODV_PU'  : 'float64',
+                         'ANGV_DEG' : 'float64',
+                         'VMXN_PU'  : 'float64',
+                         'VMNN_PU'  : 'float64',
+                         'OWN'      : 'int32',
+                         'SUB'      : 'int32'}
 
         self.DF_bus = self.DF_bus.astype(type_dict)
 
@@ -76,6 +102,10 @@ class NetworkData():
     def getLoadDataFrame(self):
 
         columns = ['BUS_ID' ,'ID', 'ST', 'PL_MW', 'QL_MVAR', 'IPL_MW', 'IQL_MVAR', 'ZPL_MW', 'ZQL_MVAR', 'OWN', 'R0', 'X0', 'Name']
+        
+        if self.other:
+            columns = ['BUS_ID' ,'ID', 'ST', 'descobrir-A', 'descobrir-B', 'PL_MW', 'QL_MVAR', 'IPL_MW', 'IQL_MVAR', 'ZPL_MW', 'ZQL_MVAR', 'OWN']
+        
         data    = []
 
         for i in range(self.f_load, self.l_load + 1):
@@ -99,6 +129,19 @@ class NetworkData():
                      'X0'       : 'float64', 
                      # 'Name'     :
                      }
+        
+        if self.other:
+            type_dict = {'BUS_ID'   : 'int32',
+                         # 'ID'       :, 
+                         'ST'       : 'int32', 
+                         'PL_MW'    : 'float64', 
+                         'QL_MVAR'  : 'float64', 
+                         'IPL_MW'   : 'float64', 
+                         'IQL_MVAR' : 'float64', 
+                         'ZPL_MW'   : 'float64', 
+                         'ZQL_MVAR' : 'float64', 
+                         'OWN'      : 'int32'}
+
 
         self.DF_load = self.DF_load.astype(type_dict)
 
@@ -114,6 +157,12 @@ class NetworkData():
                    'BASE_MVA', 'RTRF_PU', 'XTRF_PU', 'TAP_PU', 'ST', 'FP%', 'PMAX_MW', 'PMIN_MW',
                    'GRP', 'BLOCKED', 'OWNER', 'CONECTION', 'R1', 'X1', 'R2', 'X2', 'R0', 'X0', 'RGRD',
                    'XGRD', 'XQ', 'SF', 'MAXANG', 'TYPE', 'NAME', 'NMAX', 'NON']
+        
+        if self.other:
+            columns = ['BUS_ID', 'ID', 'PG_MW', 'QG_MVAR', 'QMX_MVAR', 'QMN_MVAR', 'VSPECPU', 'BCO_ID',
+                       'BASE_MVA', 'RTRF_PU', 'XTRF_PU', 'TAP_PU', 'ST', 'FP%', 'PMAX_MW', 'PMIN_MW',
+                       'GRP', 'BLOCKED']
+        
         data    = []
 
         for i in range(self.f_gen, self.l_gen + 1):
@@ -159,12 +208,32 @@ class NetworkData():
                      'NMAX'      : 'int32', 
                      'NON'       : 'int32'
                      }
+        
+        if self.other:
+            type_dict = {'BUS_ID'    : 'int32',
+                         # 'ID'        :, 
+                         'PG_MW'     : 'float64', 
+                         'QG_MVAR'   : 'float64', 
+                         'QMX_MVAR'  : 'float64', 
+                         'QMN_MVAR'  : 'float64', 
+                         'VSPECPU'   : 'float64', 
+                         'BCO_ID'    : 'int32',
+                         'BASE_MVA'  : 'float64', 
+                         'RTRF_PU'   : 'float64', 
+                         'XTRF_PU'   : 'float64', 
+                         'TAP_PU'    : 'float64', 
+                         'ST'        : 'int32', 
+                         'FP%'       : 'float64', 
+                         'PMAX_MW'   : 'float64', 
+                         'PMIN_MW'   : 'float64',
+                         'GRP'       : 'int32', 
+                         'BLOCKED'   : 'int32'}
 
         self.DF_gen = self.DF_gen.astype(type_dict)
 
         self.DF_gen['PMAX_MW'] = self.DF_gen['PG_MW']*1.25
 
-    def networkInfo(self):
+    def networkInfo(self, show=False):
 
         self.total_PL_MW   = self.DF_load['PL_MW'].sum()   
         self.total_QL_MVAR = self.DF_load['QL_MVAR'].sum() 
@@ -185,8 +254,39 @@ class NetworkData():
         self.total_PG_SYNC  = self.total_PG_MW - self.total_PG_RENEW
         # print(self.total_PG_RENEW, self.total_PG_SYNC)
 
-        self.total_PMAX_MW_unblock = self.DF_gen[self.DF_gen['BLOCKED'] == 0]['PMAX_MW'].sum() 
-        print('UNBLOCKED:', self.total_PMAX_MW_unblock)
+        self.total_PMAX_MW_unblock  = self.DF_gen[self.DF_gen['BLOCKED'] == 0]['PMAX_MW'].sum() 
+        self.total_PG_MW_unblock    = self.DF_gen[self.DF_gen['BLOCKED'] == 0]['PG_MW'].sum() 
+        
+        self.total_headroom_unblock = self.total_PMAX_MW_unblock - self.total_PG_MW_unblock
+        
+        
+        self.total_headroom        = self.total_PMAX_MW - self.total_PG_MW
+        # print('UNBLOCKED:', self.total_PMAX_MW_unblock)
+        
+        self.penetration = self.total_PG_RENEW / self.total_PG_MW
+        
+        if show:
+            
+            print(' ====================== Network Info ====================== ')
+            print(' ======================== GENERATOR ======================= \n')
+            print(f' Total Active Power:   {self.total_PG_MW:.4f} MW')
+            print(f' Total Reactive Power: {self.total_QG_MVAR:.4f} MVar\n')
+            
+            print(f' Total Max Active Power: {self.total_PMAX_MW:.4f} MW')
+            print(f' Total Min Active Power: {self.total_PMIN_MW:.4f} MW\n')
+            
+            print(f' Total Renew Active Power: {self.total_PG_RENEW:.4f} MW')
+            print(f' Total Sync Active Power:  {self.total_PG_SYNC:.4f} MW\n')
+            
+            print(f' Total Headroom:             {self.total_headroom:.4f} MW')
+            print(f' Total Max Unblocked Power:  {self.total_PMAX_MW_unblock:.4f} MW')
+            print(f' Total Head Unblocked Power: {self.total_headroom_unblock:.4f} MW\n')
+            
+            print(f' Renweable Penetration: {self.penetration*100:.4f} %\n')
+            
+            print(' ========================== LOAD ========================== \n')
+            print(f' Total Active Power:   {self.total_PL_MW:.4f} MW')
+            print(f' Total Reactive Power: {self.total_QL_MVAR:.4f} MVar\n\n')
 
     def changeLoad(self, param, multi, spec=None, arre=2, keepFP=False):
         
@@ -194,15 +294,27 @@ class NetworkData():
         ite = spec if spec else [i for i in range(len(self.DF_load))]
         
         if keepFP:
-            for i in ite:                    
-                MW, MVAR = float(self.DF_load['PL_MW'][i]), float(self.DF_load['QL_MVAR'][i])                    
-                FP       = MW / np.sqrt(MW**2 + MVAR**2)
+            for i in ite:      
+                
+                kw, kvar = self.DF_load['PL_MW'].values[i], self.DF_load['QL_MVAR'].values[i]
+                FP       = np.cos(np.arctan(kvar/kw))
+                
+                kw_new   = kw*multi
+                kvar_new = np.tan(np.arccos(FP))*kw_new
+                
+                self.DF_load.loc[i, 'PL_MW']   = kw_new
+                self.DF_load.loc[i, 'QL_MVAR'] = kvar_new
+                
+                
+                
+#                 MW, MVAR = float(self.DF_load['PL_MW'][i]), float(self.DF_load['QL_MVAR'][i])                    
+#                 FP       = MW / np.sqrt(MW**2 + MVAR**2)
                     
-                new_MW   = MW*multi
-                new_MVAR = np.sqrt((new_MW/FP)**2 - new_MW**2)
+#                 new_MW   = MW*multi
+#                 new_MVAR = np.sqrt((new_MW/FP)**2 - new_MW**2)
                     
-                self.DF_load['PL_MW'][i]   = round(new_MW,   arre) 
-                self.DF_load['QL_MVAR'][i] = round(new_MVAR, arre)                  
+#                 self.DF_load['PL_MW'][i]   = round(new_MW,   arre) 
+#                 self.DF_load['QL_MVAR'][i] = round(new_MVAR, arre)                  
 
         else:      
             for i in ite:
@@ -211,6 +323,9 @@ class NetworkData():
         self.cargaTotal = 0
         for i in self.DF_load['PL_MW'].values:
             self.cargaTotal += float(i)
+            
+    def changeGenType(self, gen_index, new_type):
+        self.DF_gen.loc[gen_index, 'TYPE'] = new_type
 
     def blockGen(self, value):
         self.networkInfo()
